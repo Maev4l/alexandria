a2705d1fd6fea75690ae0d53e74ca018e6ca3b11
Maev4l <maeval.nightingale@gmail.com>
2024-03-17 15:52:25 +0100

    Change UI of library items in the flat list

--- packages/web-client/src/libraries/ItemsList.jsx
+++ packages/web-client/src/libraries/ItemsList.jsx
@@ -1,4 +1,4 @@
-import { Text, useTheme, IconButton, Icon } from 'react-native-paper';
+import { Text, useTheme, IconButton, Icon, Divider } from 'react-native-paper';
 import { FlatList, View, Image, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useActionSheet } from '@expo/react-native-action-sheet';
@@ -9,80 +9,85 @@ import { ITEM_TYPE } from '../domain';
 import { useDispatch } from '../store';
 import { deleteLibraryItem } from './operations';
 
-const BookItem = ({ book, style, onPress, onPressActions }) => {
+const BookItem = ({ book, style, onPress, onPressActions, showDivider }) => {
   const theme = useTheme();
   const { title, authors, isbn, picture } = book;
   return (
-    <Pressable onPress={onPress}>
-      <View
-        style={{
-          flex: 1,
-          borderWidth: 2,
+    <>
+      <Pressable onPress={onPress}>
+        <View
+          style={{
+            flex: 1,
+            /* borderWidth: 2,
           borderRadius: '5px',
           borderColor: theme.colors.secondary,
-          padding: 5,
-          minHeight: '100px',
-          ...style,
-        }}
-      >
-        <View style={{ flex: 1, flexDirection: 'row' }}>
+          padding: 5, */
+            minHeight: '100px',
+            ...style,
+          }}
+        >
           <View style={{ flex: 1, flexDirection: 'row' }}>
-            {picture ? (
-              <View
-                style={{
-                  width: 60,
-                  height: 90,
-                }}
-              >
-                <Image
-                  source={{
-                    uri: `data:image/jpeg;base64,${picture}`,
-                  }}
+            <View style={{ flex: 1, flexDirection: 'row' }}>
+              {picture ? (
+                <View
                   style={{
-                    resizeMode: 'stretch',
-                    flex: 1,
-                    width: '100%',
-                    height: '100%',
-                    borderRadius: '5%',
+                    width: 60,
+                    height: 90,
                   }}
-                />
-              </View>
-            ) : (
+                >
+                  <Image
+                    source={{
+                      uri: `data:image/jpeg;base64,${picture}`,
+                    }}
+                    style={{
+                      resizeMode: 'stretch',
+                      flex: 1,
+                      width: '100%',
+                      height: '100%',
+                      borderRadius: '5%',
+                    }}
+                  />
+                </View>
+              ) : (
+                <View
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    width: 60,
+                    height: 90,
+                    borderWidth: '1px',
+                    borderRadius: '5%',
+                    borderColor: theme.colors.primary,
+                  }}
+                >
+                  <Text variant="titleLarge">?</Text>
+                </View>
+              )}
               <View
-                style={{
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  width: 60,
-                  height: 90,
-                  borderWidth: '1px',
-                  borderRadius: '5%',
-                  borderColor: theme.colors.primary,
-                }}
+                style={{ flex: 1, height: 90, paddingLeft: 5, justifyContent: 'space-between' }}
               >
-                <Text variant="titleLarge">?</Text>
+                <View style={{ flexShrink: 1 }}>
+                  <Text variant="labelLarge" style={{ flexWrap: 'wrap' }}>
+                    {title}
+                  </Text>
+                  <Text style={{ fontStyle: 'italic' }}>{authors.join(', ')}</Text>
+                </View>
+                <Text>ISBN: {isbn}</Text>
               </View>
-            )}
-            <View style={{ flex: 1, height: 90, paddingLeft: 5, justifyContent: 'space-between' }}>
-              <View style={{ flexShrink: 1 }}>
-                <Text variant="labelLarge" style={{ flexWrap: 'wrap' }}>
-                  {title}
-                </Text>
-                <Text style={{ fontStyle: 'italic' }}>{authors.join(', ')}</Text>
-              </View>
-              <Text>ISBN: {isbn}</Text>
             </View>
+            <IconButton
+              icon="dots-vertical"
+              animated
+              size={16}
+              mode="contained"
+              onPress={onPressActions}
+              style={{ marginTop: 0 }}
+            />
           </View>
-          <IconButton
-            icon="dots-vertical"
-            animated
-            size={16}
-            mode="contained"
-            onPress={onPressActions}
-            style={{ marginTop: 0 }}
-          />
         </View>
-      </View>
-    </Pressable>
+      </Pressable>
+      {showDivider ? <Divider style={{ marginBottom: 10 }} horizontalInset /> : null}
+    </>
   );
 };
 
@@ -137,13 +142,13 @@ const ItemsList = ({ library, items, onEndReached, onRefresh, refreshing }) => {
       ListEmptyComponent={() => (
         <Alert variant="primary" style={{ marginTop: 20 }} text="You have no items." />
       )}
-      renderItem={({ item }) =>
+      renderItem={({ item, index }) =>
         item.type === ITEM_TYPE.BOOK ? (
           <BookItem
             book={item}
-            style={{ marginBottom: 10 }}
             onPress={() => navigation.navigate('BookDetails', { book: { ...item } })}
             onPressActions={() => handlePressActions(item)}
+            showDivider={index !== items.length - 1}
           />
         ) : null
       }
