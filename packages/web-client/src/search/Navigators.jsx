36f4325434fc440b3d7af07d9c7a0d9fde2565da
Maev4l <maeval.nightingale@gmail.com>
2024-05-17 16:56:26 +0200

    fix: Cannot mutate any items from the search screen

--- packages/web-client/src/search/Navigators.jsx
+++ packages/web-client/src/search/Navigators.jsx
@@ -1,21 +1,14 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { IconButton } from 'react-native-paper';
 
-import {
-  BookDetailsHeader,
-  BookDetails,
-  ItemHistoryHeader,
-  ItemHistory,
-  deleteLibraryItemHistory,
-} from '../libraries';
+import { BookDetailsHeader, BookDetails, ItemHistoryHeader, ItemHistory } from '../libraries';
 import SearchScreen from './SearchScreen';
-import { useAuth, useDispatch, useSelector } from '../store';
+import { useAuth } from '../store';
 
 const Stack = createStackNavigator();
 
 const Navigator = () => {
   const { userId } = useAuth();
-  const dispatch = useDispatch();
   return (
     <Stack.Navigator
       screenOptions={{ animationEnabled: true, headerTitleAlign: 'center' }}
@@ -51,22 +44,6 @@ const Navigator = () => {
         name="ItemHistory"
         options={({ route }) => ({
           headerTitle: () => <ItemHistoryHeader route={route} />,
-          headerRight: () => {
-            const {
-              params: { item },
-            } = route;
-
-            const { lentTo } = item;
-
-            const eventsCount = useSelector((state) => state.itemHistory.events.length);
-            return eventsCount !== 0 && !lentTo ? (
-              <IconButton
-                icon="trash-can-outline"
-                size={24}
-                onPress={() => dispatch(deleteLibraryItemHistory(item))}
-              />
-            ) : null;
-          },
         })}
         component={ItemHistory}
       />
