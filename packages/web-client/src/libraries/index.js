6dae5484253f24d873b3b6d2a5e283a5b4f84528
Maev4l <maeval.nightingale@gmail.com>
2024-05-17 16:31:33 +0200

    fix: Fix navigation from event history screen

--- packages/web-client/src/libraries/index.js
+++ packages/web-client/src/libraries/index.js
@@ -1,3 +1,6 @@
 export { default as LibrariesNavigators } from './Navigators';
 export * from './BookDetails';
 export { default as BookDetails } from './BookDetails';
+export * from './ItemHistory';
+export { default as ItemHistory } from './ItemHistory';
+export * from './operations';
--- packages/web-client/src/search/Navigators.jsx
+++ packages/web-client/src/search/Navigators.jsx
@@ -1,14 +1,21 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { IconButton } from 'react-native-paper';
 
-import { BookDetailsHeader, BookDetails } from '../libraries';
+import {
+  BookDetailsHeader,
+  BookDetails,
+  ItemHistoryHeader,
+  ItemHistory,
+  deleteLibraryItemHistory,
+} from '../libraries';
 import SearchScreen from './SearchScreen';
-import { useAuth } from '../store';
+import { useAuth, useDispatch, useSelector } from '../store';
 
 const Stack = createStackNavigator();
 
 const Navigator = () => {
   const { userId } = useAuth();
+  const dispatch = useDispatch();
   return (
     <Stack.Navigator
       screenOptions={{ animationEnabled: true, headerTitleAlign: 'center' }}
@@ -39,6 +46,29 @@ const Navigator = () => {
           };
         }}
         component={BookDetails}
+      />
+      <Stack.Screen
+        name="ItemHistory"
+        options={({ route }) => ({
+          headerTitle: () => <ItemHistoryHeader route={route} />,
+          headerRight: () => {
+            const {
+              params: { item },
+            } = route;
+
+            const { lentTo } = item;
+
+            const eventsCount = useSelector((state) => state.itemHistory.events.length);
+            return eventsCount !== 0 && !lentTo ? (
+              <IconButton
+                icon="trash-can-outline"
+                size={24}
+                onPress={() => dispatch(deleteLibraryItemHistory(item))}
+              />
+            ) : null;
+          },
+        })}
+        component={ItemHistory}
       />
     </Stack.Navigator>
   );
